ece4688b0969b3ec0dcb91cc42a96f40a35dcc15
pjh <kin91210@naver.com>
2023-02-02 20:10:00 +0900

    백준 1065

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,16 @@
-# 백준 5597
-s = [i for i in range(1, 31)]
+# 백준 1065
+X = input()
 
-for i in range(28):
-    s.remove(int(input()))
-print(s[0])
-print(s[1])
+count = 0
+
+if len(X) <= 2:
+    count = X
+    
+elif len(X) >= 3:
+    count += 99
+    for i in range(100, int(X)+1):
+        a = int(str(i)[0]) - int(str(i)[1])
+        b = int(str(i)[1]) - int(str(i)[2])
+        if a == b:
+            count +=1
+print(count)
